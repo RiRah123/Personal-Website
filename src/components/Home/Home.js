f45e6ff907d9c8cd52945b24da530bf3c6b9e70a
Rian Rahman <rian.rahman@gatech.edu>
2022-09-13 03:40:59 -0400

    Added starting base for logo

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,22 +13,15 @@ function Home() {
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
-                </span>
+                Hello There!{" "} <span className="wave" role="img" aria-labelledby="wave">👋🏻</span>
               </h1>
-
               <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> Rian Rahman</strong>
+                I'M <strong className="main-name"> Rian Rahman</strong>
               </h1>
-
               <div style={{ padding: 50, textAlign: "left" }}>
                 <Type />
               </div>
             </Col>
-
             <Col md={5} style={{ paddingBottom: 120 }}>
               <img
                 src={homeLogo}
