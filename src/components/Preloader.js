5de7a964a3276995fb3b520bd22454354c75addc
Rahman <rian.rahman@hp.com>
2023-05-30 00:45:23 -0600

    Changed preloader from three dots to a lottie laptop animation

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -1,6 +1,23 @@
 import React from "react";
-function Pre(props) {
-  return <div id={props.load ? "preloader" : "preloader-none"}></div>;
+import Lottie from "react-lottie";
+import codingAnimation from "./../../src/Assets/preloader.json";
+
+function Preloader({ load }) {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: codingAnimation,
+  };
+
+  return (
+    <div id={load ? "preloader" : "preloader-none"} className="preloader-container">
+      {load ? (
+        <div className="animation-container">
+          <Lottie options={defaultOptions} />
+        </div>
+      ) : null}
+    </div>
+  );
 }
 
-export default Pre;
+export default Preloader;
